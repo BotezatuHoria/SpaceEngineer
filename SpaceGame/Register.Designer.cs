5d3a502c4710ba53248f9e2a80a5931df364fb59
Botezatu Horia <horia12btz@gmail.com>
2023-04-27 15:25:19 +0300

    Register Redesign

--- SpaceGame/Register.Designer.cs
+++ SpaceGame/Register.Designer.cs
@@ -35,113 +35,62 @@ namespace SpaceGame
             this.emailBox = new System.Windows.Forms.TextBox();
             this.passwordBox = new System.Windows.Forms.TextBox();
             this.registerButton = new System.Windows.Forms.Button();
-            this.fnameLabel = new System.Windows.Forms.Label();
-            this.snameLabel = new System.Windows.Forms.Label();
-            this.emailLabel = new System.Windows.Forms.Label();
-            this.passwordLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // fNameBox
             // 
-            this.fNameBox.BackColor = System.Drawing.Color.SeaShell;
+            this.fNameBox.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.fNameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.fNameBox.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.fNameBox.Location = new System.Drawing.Point(282, 142);
+            this.fNameBox.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fNameBox.Location = new System.Drawing.Point(511, 208);
             this.fNameBox.Name = "fNameBox";
-            this.fNameBox.Size = new System.Drawing.Size(263, 30);
+            this.fNameBox.Size = new System.Drawing.Size(263, 41);
             this.fNameBox.TabIndex = 2;
             // 
             // sNameBox
             // 
-            this.sNameBox.BackColor = System.Drawing.Color.SeaShell;
+            this.sNameBox.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.sNameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.sNameBox.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.sNameBox.Location = new System.Drawing.Point(282, 93);
+            this.sNameBox.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sNameBox.Location = new System.Drawing.Point(511, 125);
             this.sNameBox.Name = "sNameBox";
-            this.sNameBox.Size = new System.Drawing.Size(263, 30);
+            this.sNameBox.Size = new System.Drawing.Size(263, 41);
             this.sNameBox.TabIndex = 1;
             // 
             // emailBox
             // 
-            this.emailBox.BackColor = System.Drawing.Color.SeaShell;
+            this.emailBox.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.emailBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.emailBox.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.emailBox.Location = new System.Drawing.Point(282, 193);
+            this.emailBox.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.emailBox.Location = new System.Drawing.Point(511, 302);
             this.emailBox.Name = "emailBox";
-            this.emailBox.Size = new System.Drawing.Size(263, 30);
+            this.emailBox.Size = new System.Drawing.Size(263, 41);
             this.emailBox.TabIndex = 3;
             // 
             // passwordBox
             // 
-            this.passwordBox.BackColor = System.Drawing.Color.SeaShell;
+            this.passwordBox.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.passwordBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.passwordBox.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.passwordBox.Location = new System.Drawing.Point(282, 249);
+            this.passwordBox.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passwordBox.Location = new System.Drawing.Point(511, 402);
             this.passwordBox.Name = "passwordBox";
             this.passwordBox.PasswordChar = '*';
-            this.passwordBox.Size = new System.Drawing.Size(263, 30);
+            this.passwordBox.Size = new System.Drawing.Size(263, 41);
             this.passwordBox.TabIndex = 4;
             // 
             // registerButton
             // 
-            this.registerButton.BackColor = System.Drawing.Color.Coral;
+            this.registerButton.BackColor = System.Drawing.Color.Transparent;
+            this.registerButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.registerButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.registerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.registerButton.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.registerButton.Location = new System.Drawing.Point(502, 519);
+            this.registerButton.Location = new System.Drawing.Point(487, 516);
             this.registerButton.Name = "registerButton";
-            this.registerButton.Size = new System.Drawing.Size(216, 67);
+            this.registerButton.Size = new System.Drawing.Size(244, 75);
             this.registerButton.TabIndex = 5;
-            this.registerButton.Text = "Register";
             this.registerButton.UseVisualStyleBackColor = false;
             this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
-            // 
-            // fnameLabel
-            // 
-            this.fnameLabel.AutoSize = true;
-            this.fnameLabel.BackColor = System.Drawing.Color.Transparent;
-            this.fnameLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.fnameLabel.ForeColor = System.Drawing.Color.Gold;
-            this.fnameLabel.Location = new System.Drawing.Point(177, 144);
-            this.fnameLabel.Name = "fnameLabel";
-            this.fnameLabel.Size = new System.Drawing.Size(90, 22);
-            this.fnameLabel.TabIndex = 5;
-            this.fnameLabel.Text = "Prenume:";
-            // 
-            // snameLabel
-            // 
-            this.snameLabel.AutoSize = true;
-            this.snameLabel.BackColor = System.Drawing.Color.Transparent;
-            this.snameLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.snameLabel.ForeColor = System.Drawing.Color.Gold;
-            this.snameLabel.Location = new System.Drawing.Point(177, 95);
-            this.snameLabel.Name = "snameLabel";
-            this.snameLabel.Size = new System.Drawing.Size(60, 22);
-            this.snameLabel.TabIndex = 6;
-            this.snameLabel.Text = "Nume:";
-            // 
-            // emailLabel
-            // 
-            this.emailLabel.AutoSize = true;
-            this.emailLabel.BackColor = System.Drawing.Color.Transparent;
-            this.emailLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.emailLabel.ForeColor = System.Drawing.Color.Gold;
-            this.emailLabel.Location = new System.Drawing.Point(177, 195);
-            this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(80, 22);
-            this.emailLabel.TabIndex = 7;
-            this.emailLabel.Text = "Email: ";
-            // 
-            // passwordLabel
-            // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
-            this.passwordLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.passwordLabel.ForeColor = System.Drawing.Color.Gold;
-            this.passwordLabel.Location = new System.Drawing.Point(177, 251);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(90, 22);
-            this.passwordLabel.TabIndex = 8;
-            this.passwordLabel.Text = "Parola: ";
             // 
             // Register
             // 
@@ -149,10 +98,6 @@ namespace SpaceGame
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SpaceGame.Properties.Resources.Register;
             this.ClientSize = new System.Drawing.Size(1237, 658);
-            this.Controls.Add(this.passwordLabel);
-            this.Controls.Add(this.emailLabel);
-            this.Controls.Add(this.snameLabel);
-            this.Controls.Add(this.fnameLabel);
             this.Controls.Add(this.registerButton);
             this.Controls.Add(this.passwordBox);
             this.Controls.Add(this.emailBox);
@@ -176,9 +121,5 @@ namespace SpaceGame
         private System.Windows.Forms.TextBox emailBox;
         private System.Windows.Forms.TextBox passwordBox;
         private System.Windows.Forms.Button registerButton;
-        private System.Windows.Forms.Label fnameLabel;
-        private System.Windows.Forms.Label snameLabel;
-        private System.Windows.Forms.Label emailLabel;
-        private System.Windows.Forms.Label passwordLabel;
     }
 }
